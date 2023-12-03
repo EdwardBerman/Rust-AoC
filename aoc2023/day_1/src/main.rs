f6d43fd090cc535c797611df5eb0b52b861f4752
EdwardBerman <10emb29@gmail.com>
2023-12-02 19:14:08 -0500

    day1

--- aoc2023/day_1/src/main.rs
+++ aoc2023/day_1/src/main.rs
@@ -1,56 +1,66 @@
 use std::fs;
 
-fn main() {
-    // Adjust the file path as necessary for your setup
-    let contents = fs::read_to_string("/home/eddieberman/aoc2023/day_1/src/input.txt")
-        .expect("Something went wrong reading the file");
-
-    // Split the file contents into lines and collect them into a Vec<String>
-    let lines: Vec<String> = contents.lines().map(|line| line.to_string()).collect();
-
-    let total = calculate_total(&lines);
-    println!("The total is: {}", total);
+fn filter_digits(input: &str) -> String {
+    input.chars().filter(|c| c.is_digit(10)).collect()
 }
 
-fn calculate_total(lines: &[String]) -> i32 {
-    let nums = vec![
-        "zero", "0", "one", "1", "two", "2", "three", "3", "four", "4", 
-        "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9",
+fn convert_substrings_to_numbers(input: &str) -> String {
+    let number_words = vec![
+        ("zero", "0"),
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
     ];
-    let mut total = 0;
 
-    for line in lines {
-        let mut first = None;
-        'out: for i in 0..line.len() {
-            for (index, num) in nums.iter().enumerate() {
-                if i + num.len() > line.len() {
-                    continue;
-                }
-                if &line[i..i + num.len()] == *num {
-                    first = Some(1 + index / 2);
-                    break 'out;
-                }
-            }
-        }
-        let first = first.expect("invalid input");
-
-        let mut last = None;
-        'out: for i in (0..line.len()).rev() {
-            for (index, num) in nums.iter().enumerate() {
-                if i + num.len() > line.len() {
-                    continue;
-                }
-                if &line[i..i + num.len()] == *num {
-                    last = Some(1 + index / 2);
-                    break 'out;
-                }
-            }
-        }
-        let last = last.expect("invalid input");
-
-        total += 10 * first as i32 + last as i32;
+    let mut converted_string = input.to_string();
+    for (word, number) in number_words {
+        let word_with_digit = insert_digit_in_middle(word, number);
+        converted_string = converted_string.replace(word, &word_with_digit);
     }
 
-    total
+    converted_string
+}
+
+fn insert_digit_in_middle(word: &str, digit: &str) -> String {
+    let middle = word.len() / 2;
+    let mut word_with_digit = String::new();
+    word_with_digit.push_str(&word[..middle]);
+    word_with_digit.push_str(digit);
+    word_with_digit.push_str(&word[middle..]);
+    word_with_digit
+}
+
+fn main() {
+    println!("{}", convert_substrings_to_numbers("eightwo"));
+    let contents = fs::read_to_string("/home/eddieberman/Rust-AoC/aoc2023/day_1/src/input.txt")
+        .expect("Something went wrong reading the file");
+
+    let lines: Vec<&str> = contents.split('\n').collect();
+    let filtered_lines: Vec<String> = lines.iter()
+        .map(|line| convert_substrings_to_numbers(line))
+        .collect();
+
+    let digits_only: Vec<String> = filtered_lines.iter()
+        .map(|line| filter_digits(line))
+        .collect();
+
+    let mut total = 0;
+
+    for line in &digits_only {
+        if !line.is_empty() {
+            let first_digit = line.chars().next().unwrap().to_digit(10).unwrap();
+            let last_digit = line.chars().last().unwrap().to_digit(10).unwrap();
+            let concatenated = format!("{}{}", first_digit, last_digit);
+            total += concatenated.parse::<i32>().unwrap();
+        }
+    }
+
+    println!("The total is: {}", total);
 }
 
